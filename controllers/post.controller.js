1d5b11ed75cd63fa4ddf275e995ae31d63c205b1
unknown <Jumpcity567@gmail.com>
2025-03-31 12:58:57 -0400

    Implemented CRUD

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -22,7 +22,77 @@ function save(req, res){
     });
 }
 
+function show(req, res){
+    const id = req.params.id;
+
+    models.Post.findByPk(id).then(result => {
+        if (result) {
+            res.status(200).json(result)
+        } else {
+            res.status(404).json({
+                message: "Post Not Found",
+            })
+        }
+    }).catch(error =>{
+        res.status(500).json({
+            message: "Something went wrong",
+        })
+    })
+}
+
+function index(req, res){
+    models.Post.findAll().then(result =>{
+        res.status(200).json(result);
+    }).catch(error =>{
+        res.status(500).json({
+            message: "Something went wrong",
+        })
+    })
+}
+
+function update(req, res){
+    const id = req.params.id;
+    const updatedPost = {
+        title: req.body.title,
+        content: req.body.content,
+        imageURL: req.body.imageURL,
+        categoryID: req.body.categoryID,
+    }
+    const userID = 1;
+
+    models.Post.update(updatedPost, {where: {id: id, userID: userID}}).then(result => {
+        res.status(200).json({
+            message: "Post Updated Successfully",
+            post: updatedPost
+        });
+    }).catch(error =>{
+        res.status(500).json({
+            message: "Something went wrong",
+            error: error
+        });
+    })
+}
+function destroy(req, res){
+    const id = req.params.id;
+    const userID = 1;
+
+    models.Post.destroy({where: {id:id, userID: userID}}).then(result => {
+        res.status(200).json({
+            message: "Post Deleted"
+        })
+    }).catch(error =>{
+        res.status(500).json({
+            message: "Something went wrong",
+            error: error
+        })
+    })
+}
+
 
 module.exports = {
-    save: save
+    save: save,
+    show: show,
+    index: index,
+    update: update,
+    destroy: destroy
 }
--- routes/post.js
+++ routes/post.js
@@ -4,5 +4,9 @@ const postcontroller = require('../controllers/post.controller');
 const router = express.Router();
 
 router.post("/", postcontroller.save);
+router.get("/:id", postcontroller.show);
+router.get("/", postcontroller.index);
+router.patch("/:id", postcontroller.update);
+router.delete("/:id", postcontroller.destroy);
 
 module.exports = router;
